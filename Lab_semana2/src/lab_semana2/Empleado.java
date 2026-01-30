afada2cbf5cd2a91f8113e8267cb8e9c42230ebc
Christopher1078 <christortizfun@gmail.com>
2026-01-30 09:24:20 -0600

    Cambios en clase empleado

--- Lab_semana2/src/lab_semana2/Empleado.java
+++ Lab_semana2/src/lab_semana2/Empleado.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab_semana2;
-
-/**
- *
- * @author USER
- */
+import java.time.LocalDate;
 public class Empleado {
+    protected String codigo, nombre;
+    protected LocalDate fecha;
+    protected double horasTrabajadas, salarioBase;
+
+    public Empleado(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.fecha=LocalDate.now();
+        this.horasTrabajadas=0;
+    }
+    
+    public void registrarHoras(double horas){
+        this.horasTrabajadas=+this.horasTrabajadas+horas;
+    }
+    
+    public double calcularPagoMensual(){
+        return (horasTrabajadas/160)*salarioBase;
+    }
+    
+    public String mostrarInfo(){
+        return "Codigo: "+codigo+"\n Nombre: "+nombre+"\n Horas trabajadas: "+horasTrabajadas;
+    }
     
 }
